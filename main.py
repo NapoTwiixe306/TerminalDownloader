cdcf071eedc608dde62454606bc6c30c3d2280b5
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-12-27 20:55:27 +0100

    delete useless commentary

--- main.py
+++ main.py
@@ -65,14 +65,3 @@ class Init:
 
 init = Init()
 init.run()
-
-# class Commands:
-#     def node():
-#         print("ici raccourcis du node install")
-
-#     def npminstall():
-#         print("raccourcis du npm install")
-
-#     def mkdircd():
-#         print("raccourcis du mkdir (avec nom de fichier) et redirige directement dans le fichier (a coup de cd)")
-
