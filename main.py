56b8a5dad392d45e648f7e221967e5a3002f11a0
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-12-27 13:37:35 +0100

    tqtpas

---
+++ main.py
@@ -0,0 +1,10 @@
+class Init:
+    def node():
+        print("ici raccourcis du node install")
+
+    def npminstall():
+        print("raccourcis du npm install")
+
+    def mkdircd():
+        print("raccourcis du mkdir (avec nom de fichier) et redirige directement dans le fichier (a coup de cd)")
+
