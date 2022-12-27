e506ac74574c9e3d4f49f59701028e536211f20d
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2022-12-27 14:46:39 +0100

    first class

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 class Init:
-    print
+    def __init__(self):
+        self.welcome_message = "Welcome NapoTwiixe !\n"
+        self.choose = "1) Install Node\n"
+        self.choose1 = "2) Install Python\n"
+        self.choose2 = "3) Mkdir && Cd\n"
 
-class Commands:
-    def node():
-        print("ici raccourcis du node install")
+    def run(self):
+        print(self.welcome_message, self.choose, self.choose1, self.choose2)
 
-    def npminstall():
-        print("raccourcis du npm install")
+        choice = input("Please choose one of the following options: 1, 2, or 3: ")
+        if choice == "1":
+            print("You chose option 1.")
+        elif choice == "2":
+            print("You chose option 2.")
+        elif choice == "3":
+            print("You chose option 3.")
+        else:
+            print("Invalid choice.")
 
-    def mkdircd():
-        print("raccourcis du mkdir (avec nom de fichier) et redirige directement dans le fichier (a coup de cd)")
+init = Init()
+init.run()
+
+# class Commands:
+#     def node():
+#         print("ici raccourcis du node install")
+
+#     def npminstall():
+#         print("raccourcis du npm install")
+
+#     def mkdircd():
+#         print("raccourcis du mkdir (avec nom de fichier) et redirige directement dans le fichier (a coup de cd)")
 
